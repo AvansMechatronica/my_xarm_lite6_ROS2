872dcb22a15d15f35889cf5a8237205490f18c12
Gerard Harkema <ga.harkema@avans.nl>
2024-11-12 20:37:09 +0100

    20241112

--- my_lite6_bringup/launch/bringup_fake.launch.py
+++ my_lite6_bringup/launch/bringup_fake.launch.py
@@ -9,6 +9,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import Command
 
 def launch_setup(context, *args, **kwargs):
 
@@ -17,7 +18,7 @@ def launch_setup(context, *args, **kwargs):
     xarm_type = ''
 
     robot_ip = LaunchConfiguration('robot_ip', default='192.168.1.162')
-    report_type = LaunchConfiguration('report_type', default='normal')
+    report_type = LaunchConfiguration('report_type', default='debug')
     
     prefix = LaunchConfiguration('prefix', default='')
     hw_ns = LaunchConfiguration('hw_ns', default='')
@@ -51,28 +52,9 @@ def launch_setup(context, *args, **kwargs):
 
     moveit_config = MoveItConfigsBuilder("lite6_robot", package_name="my_lite6_moveit_config").to_moveit_configs()
 
-    if 0:
-        lite6_planner_realmove_path = os.path.join(
-            get_package_share_directory('xarm_planner'),
-            'launch',
-            'lite6_planner_fake.launch.py'  # assuming converted to Python
-        )
-        lite6_planner_realmove_include = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(lite6_planner_realmove_path),
-            #launch_arguments={'robot_ip': LaunchConfiguration('robot_ip')}.items()
-            launch_arguments={'robot_ip': '192.168.1.162'}.items()
-        )
+    #xacro_file = LaunchConfiguration('xacro_file', default=PathJoinSubstitution([FindPackageShare('xarm_description'), 'urdf', 'xarm_device.urdf.xacro']))
+    xacro_file = LaunchConfiguration('xacro_file', default=PathJoinSubstitution([FindPackageShare('my_lite6_description'), 'urdf', 'lite6_robot.urdf.xacro']))
 
-        lite6_planner_fake_path = os.path.join(
-            get_package_share_directory('xarm_planner'),
-            'launch',
-            'lite6_planner_fake.launch.py'  # assuming converted to Python
-        )
-        lite6_planner_fake_include = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(lite6_planner_fake_path)
-        )
-
-    xacro_file = LaunchConfiguration('xacro_file', default=PathJoinSubstitution([FindPackageShare('xarm_description'), 'urdf', 'xarm_device.urdf.xacro']))
 
     # robot ros2 control launch
     # xarm_controller/launch/_robot_ros2_control.launch.py
@@ -80,7 +62,7 @@ def launch_setup(context, *args, **kwargs):
         PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare('xarm_controller'), 'launch', '_robot_ros2_control.launch.py'])),
         launch_arguments={
             'robot_ip': robot_ip,
-#            'report_type': report_type,
+            'report_type': report_type,
 #            'prefix': prefix,
             'hw_ns': hw_ns,
 #            'limited': limited,
@@ -108,19 +90,6 @@ def launch_setup(context, *args, **kwargs):
         }.items(),
     )
 
-
-
-    lite6_driver_path = os.path.join(
-        get_package_share_directory('xarm_api'),
-        'launch',
-        'lite6_driver.launch.py'  # assuming converted to Python
-    )
-    lite6_driver_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(lite6_driver_path),
-        #launch_arguments={'robot_ip': LaunchConfiguration('robot_ip')}.items()
-        launch_arguments={'robot_ip': '192.168.1.162'}.items()
-    )
-
     move_group_path = os.path.join(
         get_package_share_directory('my_lite6_moveit_config'),
         'launch',
@@ -129,25 +98,6 @@ def launch_setup(context, *args, **kwargs):
     move_group_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(move_group_path),
     )
-
-    spawn_controllers_path = os.path.join(
-        get_package_share_directory('my_lite6_moveit_config'),
-        'launch',
-        'spawn_controllers.launch.py'  # assuming converted to Python
-    )
-    spawn_controllers_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(spawn_controllers_path),
-    )
-
-    if 0:
-        moveit_rviz_path = os.path.join(
-            get_package_share_directory('my_lite6_moveit_config'),
-            'launch',
-            'moveit_rviz.launch.py'  # assuming converted to Python
-        )
-        moveit_rviz_include = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(moveit_rviz_path),
-        )
 
     # RViz
     rviz_config_file = (
@@ -180,9 +130,7 @@ def launch_setup(context, *args, **kwargs):
         ],
     )
 
-
     controllers = [
-#         '{}{}_traj_controller'.format(prefix.perform(context), xarm_type),
         'lite6_traj_controller',
     ]
 
@@ -193,24 +141,33 @@ def launch_setup(context, *args, **kwargs):
             package='controller_manager',
             executable='spawner',
             output='screen',
-            remappings=[
-                ('follow_joint_trajectory', '/arm_controller/follow_joint_trajectory')
-            ],
             arguments=[
                 controller,
                 '--controller-manager', '{}/controller_manager'.format(ros_namespace)
             ],
+            parameters=[{'use_sim_time': False}],
         ))
 
+    urdf_file_path = os.path.join(
+        get_package_share_directory('my_lite6_description'),
+        'urdf',
+        'lite6_robot.urdf.xacro'
+    )
+    # Define the robot description using xacro command
+    robot_description = Command(['xacro ', urdf_file_path])
 
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
 
     return[
-            #lite6_planner_fake_include,
-            #lite6_driver_include,
             robot_ros2_control_launch,
-            #spawn_controllers_include,
+            robot_state_publisher_node,
+            joint_state_broadcaster,
             move_group_include,
-            #moveit_rviz_include,
             rviz_node,
         ]  + controller_nodes
 
